a72bc58ba7bb90e783018b3cbd34bd37902e8df6
VictorLi <87053334313@mail.ru>
2021-02-04 19:35:21 +0300

    добавил что хотел

--- Calculator_Victor_Li/WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
+++ Calculator_Victor_Li/WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
@@ -68,16 +68,18 @@ namespace WindowsFormsApp1
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(293, 75);
+            this.label2.ForeColor = System.Drawing.Color.Firebrick;
+            this.label2.Location = new System.Drawing.Point(254, 72);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(203, 17);
+            this.label2.Size = new System.Drawing.Size(269, 17);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Введи значение для опреции";
+            this.label2.Text = "Введи числовое значение для опреции";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.PaleGreen;
-            this.button1.Location = new System.Drawing.Point(58, 213);
+            this.button1.Location = new System.Drawing.Point(90, 270);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 46);
             this.button1.TabIndex = 3;
@@ -111,17 +113,18 @@ namespace WindowsFormsApp1
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(33, 154);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Firebrick;
+            this.label4.Location = new System.Drawing.Point(64, 170);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(179, 30);
+            this.label4.Size = new System.Drawing.Size(207, 30);
             this.label4.TabIndex = 6;
             this.label4.Text = "Введи число в какую степень \n хочешь возвести";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // TBstepen
             // 
-            this.TBstepen.Location = new System.Drawing.Point(36, 187);
+            this.TBstepen.Location = new System.Drawing.Point(67, 223);
             this.TBstepen.Name = "TBstepen";
             this.TBstepen.Size = new System.Drawing.Size(136, 20);
             this.TBstepen.TabIndex = 7;
@@ -131,7 +134,7 @@ namespace WindowsFormsApp1
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label5.ForeColor = System.Drawing.Color.Crimson;
-            this.label5.Location = new System.Drawing.Point(260, 160);
+            this.label5.Location = new System.Drawing.Point(560, 170);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(247, 39);
             this.label5.TabIndex = 8;
@@ -141,7 +144,7 @@ namespace WindowsFormsApp1
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Lime;
-            this.button2.Location = new System.Drawing.Point(296, 238);
+            this.button2.Location = new System.Drawing.Point(595, 270);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(189, 67);
             this.button2.TabIndex = 10;
@@ -159,7 +162,7 @@ namespace WindowsFormsApp1
             "Определение квадрата числа",
             "Расчет факториала",
             "Извлечение кубического корня"});
-            this.switchOp.Location = new System.Drawing.Point(243, 202);
+            this.switchOp.Location = new System.Drawing.Point(547, 223);
             this.switchOp.Name = "switchOp";
             this.switchOp.Size = new System.Drawing.Size(290, 21);
             this.switchOp.TabIndex = 11;
@@ -180,7 +183,7 @@ namespace WindowsFormsApp1
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.button4.Location = new System.Drawing.Point(685, 75);
+            this.button4.Location = new System.Drawing.Point(320, 170);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 74);
             this.button4.TabIndex = 13;
--- Calculator_Victor_Li/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
+++ Calculator_Victor_Li/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -133,5 +133,10 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
